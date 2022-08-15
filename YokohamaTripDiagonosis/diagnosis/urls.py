4a0f1400091e105ba17d7a4a58d814b1efbb9768
M4SHUMAR0 <kiyoto20020218@gmail.com>
2022-08-15 21:54:26 +0900

    【バグ修正中】resultの動的ページ作成

--- YokohamaTripDiagonosis/diagnosis/urls.py
+++ YokohamaTripDiagonosis/diagnosis/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('result1/',views.result1,name="result1"),
     path('result2/',views.result2,name="result2"),
     path('result3/',views.result3,name="result3"),
-    path('result4',views.result4,name="result4"),   
+    path('result4/',views.result4,name="result4"),   
     path('result/',views.result,name="result"), 
 ]
 
--- YokohamaTripDiagonosis/diagnosis/views.py
+++ YokohamaTripDiagonosis/diagnosis/views.py
@@ -12,19 +12,36 @@ def result(request):
     personality=request.GET.get("personality")
     pace=request.GET.get("pace")
     member=request.GET.get("member")
-    return render(request,"templates/diagnosis.html",{"price":price,"personality":personality,"pace":pace,"member":member})
+    if price=="rich":
+        grade_price=1
+    else:
+        grade_price=0
+    
+    if personality=="outdoor":
+        grade_personality=2
+    elif personality=="middle":
+        grade_personality=1
+    else:
+        grade_personality=0
+    
+    if pace=="quick":
+        grade_pace=4
+    else:
+        grade_pace=0
+    total=grade_price+grade_personality+grade_pace
+    return render(request,"templates/diagnosis.html",{"price":price,"personality":personality,"pace":pace,"member":member,"total":total})
 
-# def result1(request):
-#     return render(request, 'result1.html')
+def result1(request):
+    return render(request, 'result1.html')
 
-# def result2(request):
-#     return render(request, 'result2.html')
+def result2(request):
+    return render(request, 'result2.html')
 
-# def result3(request):
-#     return render(request, 'result3.html')
+def result3(request):
+    return render(request, 'result3.html')
 
-# def result4(request):
-#     return render(request, 'result4.html')
+def result4(request):
+    return render(request, 'result4.html')
 
 # def judge_result(request):
 #     price=request.GET.get("price")
