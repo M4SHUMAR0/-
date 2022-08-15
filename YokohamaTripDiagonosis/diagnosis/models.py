968056d4cd2c95751e6966d0ac0ae5424410ceed
M4SHUMAR0 <88787143+M4SHUMAR0@users.noreply.github.com>
2022-08-15 20:38:16 +0900

    Revert "Createapp"

--- YokohamaTripDiagonosis/diagnosis/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-# Create your models here.
-class evaluation(models.Model):
-    created_time=models.DateTimeField(default=timezone.now)
-    evaluation=models.IntegerField()
--- YokohamaTripDiagonosis/diagnosis/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import views
- 
-urlpatterns = [
-    re_path(r'^$', views.judge_result, name='judge_result'),
-]
